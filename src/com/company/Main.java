3946cdff903c304b088a031feff0f21750b86ddc
tulipan <modrzejewskimichal@wp.pl>
2015-11-23 11:24:14 +0100

    kommit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double a, b, c, d, e,f,g;
+        double a, b, c, d, e, f, g;
         System.out.println("Obliczanie raty kredytu Zakres kwoty od 100 do 10000pln, " +
                 "oprocentowanie zmienne 6-12rat=2.5%, 13-24rat=5%, 25-48rat=10%");
-        System.out.println("Prosze podac kwote");
-        Scanner Kw = new Scanner(System.in);
-        b = Kw.nextInt();
-        System.out.println("Prosze podac liczbe rat");
-        Scanner LR = new Scanner(System.in);
-        a = LR.nextInt();
-        while (a < 6 || a > 48 || b < 100 || b > 10000) {
-            System.out.println("Podane wartości są poza zakresem, sprubuj ponownie");
+        do {
             System.out.println("Prosze podac kwote");
-            Scanner Kw1 = new Scanner(System.in);
-            f = Kw1.nextInt();
+            Scanner Kw = new Scanner(System.in);
+            b = Kw.nextInt();
             System.out.println("Prosze podac liczbe rat");
-            Scanner LR1 = new Scanner(System.in);
-            g = LR1.nextInt();
-            a=f;
-            b=g;
+            Scanner LR = new Scanner(System.in);
+            a = LR.nextInt();
+            if (a < 6 || a > 48 || b < 100 || b > 10000) {
+                System.out.println("Dane z poza oferty, proszę skorygować dane");
+            }
         }
+        while (a < 6 || a > 49 || b < 100 || b > 10000);
         if (a >= 6 && a <= 12) {
             c = (b * 1.025) / a;
             System.out.println(c + "PLN");
@@ -34,10 +29,7 @@ public class Main {
         } else if (a >= 25 && a <= 48) {
             e = (b * 1.1) / a;
             System.out.println(e + "PLN");
-        } else {
-
         }
-
         //cena towaru 100-10tys zł
         //oprocentowanie 6-12rat=2.5%, 13-24rat=5%, 25-48rat=10%
         // write your code here
